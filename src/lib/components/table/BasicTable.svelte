795b2fb062b475895655dbf5187a08f99bfac30b
Omar Carrillo <124636738+OmarCarrillo1121@users.noreply.github.com>
2025-10-06 21:33:24 -0600

    Creacion de tabla

--- src/lib/components/table/BasicTable.svelte
+++ src/lib/components/table/BasicTable.svelte
@@ -3,9 +3,31 @@
 </script>
 
 <!-- AQUI INICIA EL HTML -->
-<span>BasicTable Works!</span>
+<table>
+    <thead>
+       <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Correo</th>
+       </tr>
+    </thead>
+    <tbody>
+       <tr>
+            <td>1</td>
+            <td>Omar</td>
+            <td>omar@mail.com</td>
+       </tr>
+       <tr>
+            <td>2</td>
+            <td>Ruben</td>
+            <td>rubens@mail.com</td>
+       </tr>
+    </tbody>
+</table>
 <!-- AQUI TERMINA EL HTML -->
 
 <style>
-    
+    thead {
+        font-size: xx-large;
+    }
 </style>
