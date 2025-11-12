9d6be22c6e83d280661cb53f1e3a8494136785e3
Omar Carrillo <124636738+OmarCarrillo1121@users.noreply.github.com>
2025-11-11 19:12:35 -0600

    Agregado componente calendario

--- src/routes/reservas/crear-reservacion/+page.svelte
+++ src/routes/reservas/crear-reservacion/+page.svelte
@@ -1,13 +1,18 @@
-<!-- <script>
+<script>
   import { ProgressStepper, Label, Input, Button } from "flowbite-svelte";
+  import SectionHeader from "$lib/components/header/SectionHeader.svelte";
+  import InputField from "$lib/components/input/InputField.svelte";
+  import SelectField from "$lib/components/input/SelectField.svelte";
+  import DateInput from "$lib/components/input/DateInput.svelte";
   import { onMount } from "svelte";
 
   let currentStep = 0;
 
-  const steps = [
-    { label: "Account", status: "current" },
-    { label: "Payment", status: "pending" },
-    { label: "Confirm", status: "pending" }
+  const pasos = [
+    { label: "Recinto y fecha", status: "current" },
+    { label: "Datos del evento", status: "pending" },
+    { label: "Recursos requeridos", status: "pending" },
+    { label: "Confirma datos", status: "pending" },
   ];
 
   function updateStepper() {
@@ -35,69 +40,61 @@
   onMount(updateStepper);
 </script>
 
-<ProgressStepper {steps} class="mb-8" />
-
-{#if currentStep === 0}
-  <form on:submit|preventDefault={nextStep}>
-    <h3 class="mb-4 text-lg font-medium">Account details</h3>
-    <div class="grid gap-4 sm:grid-cols-2 mb-4">
-      <div>
-        <Label for="username">Username</Label>
-        <Input id="username" name="username" placeholder="username.example" required />
-      </div>
-      <div>
-        <Label for="email">Email</Label>
-        <Input id="email" type="email" placeholder="name@company.com" required />
-      </div>
-      <div>
-        <Label for="password">Password</Label>
-        <Input id="password" type="password" placeholder="••••••••" required />
-      </div>
-      <div>
-        <Label for="confirm-password">Confirm password</Label>
-        <Input id="confirm-password" type="password" placeholder="••••••••" required />
-      </div>
+<div class="container">
+  <SectionHeader titulo={`Crear reservación`} />
+  <div class="contenedor-componente">
+    <div class="contenedor-stepper">
+      <ProgressStepper {pasos} class="mb-8" />
     </div>
-    <Button type="submit">Next Step: Payment Info</Button>
-  </form>
-{/if}
+    <div class="contenedor-form">
+      {#if currentStep === 0}
+        <div class="form-step">
+          <form on:submit|preventDefault={nextStep}>
+            S
+            <Button type="submit">Next Step: Payment Info</Button>
+          </form>
+        </div>
+      {/if}
 
-{#if currentStep === 1}
-  <form on:submit|preventDefault={nextStep}>
-    <h3 class="mb-4 text-lg font-medium">Payment Info</h3>
-    <div class="grid gap-4 sm:grid-cols-2 mb-4">
-      <div>
-        <Label for="card">Card Number</Label>
-        <Input id="card" placeholder="1234 5678 9012 3456" required />
-      </div>
-      <div>
-        <Label for="expiry">Expiry Date</Label>
-        <Input id="expiry" placeholder="MM/YY" required />
-      </div>
-      <div>
-        <Label for="cvv">CVV</Label>
-        <Input id="cvv" placeholder="123" required />
-      </div>
-    </div>
-    <div class="flex justify-between">
-      <Button color="gray" type="button" on:click={prevStep}>Back</Button>
-      <Button type="submit">Next Step: Confirm</Button>
-    </div>
-  </form>
-{/if}
+      {#if currentStep === 1}
+        <form on:submit|preventDefault={nextStep}>
+          <h3 class="mb-4 text-lg font-medium">Payment Info</h3>
+          <div class="grid gap-4 sm:grid-cols-2 mb-4">
+            <div>
+              <Label for="card">Card Number</Label>
+              <Input id="card" placeholder="1234 5678 9012 3456" required />
+            </div>
+            <div>
+              <Label for="expiry">Expiry Date</Label>
+              <Input id="expiry" placeholder="MM/YY" required />
+            </div>
+            <div>
+              <Label for="cvv">CVV</Label>
+              <Input id="cvv" placeholder="123" required />
+            </div>
+          </div>
+          <div class="flex justify-between">
+            <Button color="gray" type="button" on:click={prevStep}>Back</Button>
+            <Button type="submit">Next Step: Confirm</Button>
+          </div>
+        </form>
+      {/if}
 
-{#if currentStep === 2}
-  <section>
-    <h3 class="mb-4 text-lg font-medium">Confirm Details</h3>
-    <p class="mb-4">Review all your information before submitting.</p>
-    <div class="flex justify-between">
-      <Button color="gray" on:click={prevStep}>Back</Button>
-      <Button color="green">Submit</Button>
+      {#if currentStep === 2}
+        <section>
+          <h3 class="mb-4 text-lg font-medium">Confirm Details</h3>
+          <p class="mb-4">Review all your information before submitting.</p>
+          <div class="flex justify-between">
+            <Button color="gray" on:click={prevStep}>Back</Button>
+            <Button color="green">Submit</Button>
+          </div>
+        </section>
+      {/if}
     </div>
-  </section>
-{/if} -->
+  </div>
+</div>
 
-<script>
+<!-- <script>
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 
@@ -133,5 +130,43 @@
 	{:else if $page.form?.error}
 		<p class="text-red-600 mt-2">❌ {$page.form.error}</p>
 	{/if}
-</form>
+</form> -->
 
+<style>
+  .contenedor-componente {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .contenedor-stepper {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    flex: 1;
+    background-color: var(--color-fondo-tarjeta);
+    height: 100%;
+    padding: 3rem;
+  }
+  .contenedor-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 2;
+    border: solid 1px var(--color-bordes);
+    height: 100%;
+    padding: 3rem;
+  }
+
+  .form-step {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    flex: 1;
+    border: solid 1px var(--color-bordes);
+  }
+</style>
